2e567108b5291a2a338dd8e6c917d07d7021815e
kevin <rwnkevinchauhan@gmail.com>
2023-09-09 12:35:37 +0530

    PR3 hero completed

--- src/PR3/Components/Hero.js
+++ src/PR3/Components/Hero.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import hero from '../hero.jpg'
+import Tabs from './Tabs'
 
 const Hero = () => {
   return (
     <div className='container py-4'>
-        <span className='text-primary subtitle'>I'm</span>
-        <h1 className='heading'>Jessica <br /> Parker</h1>
-        <p>A passionate UI/UX Designer and Web Developer based In NYC, USA</p>
+      <div className="row">
+        <div className="col-6">
+          <span className='text-primary subtitle'>I'm</span>
+          <h1 className='heading'>Jessica <br /> Parker</h1>
+          <p>A passionate UI/UX Designer and Web Developer based In NYC, USA</p>
+          <button className='btn btn-warning text-white rounded-circle hero-play-btn'>play</button>
+          <span className='fw-bold fs-5 ms-3'>Play Video</span>
+        </div>
+
+        <div className="col-6 hero-img">
+          <div className="ms-auto">
+            <img className='rounded-4' src={hero} alt="" width={'100%'} />
+          </div>
+        </div>
+      </div>
+      <Tabs/>
     </div>
   )
 }
---
+++ src/PR3/Components/Tabs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Tabs = () => {
+  return (
+    <nav>
+        <ul>
+            <li><a href="#"><i className="ri-file-list-3-line"></i></a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Tabs
