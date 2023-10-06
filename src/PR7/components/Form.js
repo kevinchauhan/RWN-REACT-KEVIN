afa1998f2ed173d567476a777b61f89bd7237714
kevin <rwnkevinchauhan@gmail.com>
2023-10-06 21:52:50 +0530

    PR7 table design

--- src/PR7/components/Form.js
+++ src/PR7/components/Form.js
@@ -48,6 +48,7 @@ const Form = () => {
         }
         setInput(initialInput)
         setCities([])
+        e.target.reset()
     }
 
     const handleEdit = (id) => {
@@ -76,104 +77,106 @@ const Form = () => {
 
     return (
         <>
-            <div className="my-10 sm:mx-auto sm:w-full px-5">
-                <form className="" onSubmit={handleSubmit} action="#" method="POST">
-                    <div className='flex flex-wrap items-end space-y-6'>
-                        <div className='w-1/2 px-3'>
-                            <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
-                            <div className="mt-2">
-                                <input id="name" name="name" type="text" autoComplete="name" required className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={input.name} onChange={handleChange} />
+            <div className="container py-10">
+                <div className="mb-10 sm:mx-auto sm:w-full px-5">
+                    <form className="" onSubmit={handleSubmit} action="#" method="POST">
+                        <div className='flex flex-wrap items-end space-y-6'>
+                            <div className='w-1/2 px-3'>
+                                <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                                <div className="mt-2">
+                                    <input id="name" name="name" type="text" autoComplete="name" required className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={input.name} onChange={handleChange} />
+                                </div>
+                            </div>
+
+                            <div className='w-1/2 px-3'>
+                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                                <div className="mt-2">
+                                    <input id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={input.email} onChange={handleChange} />
+                                </div>
+                            </div>
+
+                            <div className='w-1/2 px-3'>
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                                <div className="mt-2">
+                                    <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 px-3 text px-3-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={input.password} onChange={handleChange} />
+                                </div>
+                            </div>
+
+                            <div className='w-1/2 px-3'>
+                                <label htmlFor="confirmPassword" className="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
+                                <div className="mt-2">
+                                    <input id="confirm-password" name="confirmPassword" type="password" required className="block w-full rounded-md border-0 py-1.5 px-3 text-g px-3ray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={input.confirmPassword} onChange={handleChange} />
+                                </div>
+                            </div>
+
+                            <div className='w-1/3 px-3'>
+                                <label htmlFor="mobile" className="block text-sm font-medium leading-6 text-gray-900">Mobile no.</label>
+                                <div className="mt-2">
+                                    <input id="mobile" name="mobile" type="text" required className="block w-full rounded-md border-0 py-1.5 px-3 text-g px-3ray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={input.mobile} onChange={handleChange} />
+                                </div>
+                            </div>
+
+                            <div className='w-1/3 px-3'>
+                                <label htmlFor="course" className="block text-sm font-medium leading-6 text-gray-900">Course</label>
+                                <div className="mt-2">
+                                    <select name='course' className='block w-full rounded-md border-0 py-1.5 px-3 text-g px-3ray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' value={input.course} onChange={handleChange} >
+                                        <option value="">--select course--</option>
+                                        <option value="be">B.E</option>
+                                        <option value="bcom">B.COM</option>
+                                        <option value="bca">BCA</option>
+                                        <option value="ba">BA</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div className='w-1/3 px-3'>
+                                <label htmlFor="gender" className="block text-sm font-medium leading-6 text-gray-900">Gender</label>
+                                <div className="mt-2">
+                                    Male
+                                    <input id="male" name="gender" value='male' type="radio" required className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600 mx-3" onChange={handleChange} />
+                                    Female
+                                    <input id="female" name="gender" value='female' type="radio" required className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600 mx-3" onChange={handleChange} />
+                                </div>
+                            </div>
+
+                            <div className='w-1/3 px-3'>
+                                <label htmlFor="state" className="block text-sm font-medium leading-6 text-gray-900">State</label>
+                                <div className="mt-2">
+                                    <DropDown input={input.state} handleOption={handleOption} states={states.states} name='state' />
+                                </div>
+                            </div>
+
+                            <div className='w-1/3 px-3'>
+                                <label htmlFor="state" className="block text-sm font-medium leading-6 text-gray-900">City</label>
+                                <div className="mt-2">
+                                    <DropDown input={input.city} handleOption={handleOption} name='city' states={cities} />
+                                </div>
+                            </div>
+
+                            <div className='w-1/3 px-3'>
+                                <label htmlFor="hobbies" className="block text-sm font-medium leading-6 text-gray-900">Hobbies</label>
+                                <div className="mt-2">
+                                    <input id="hobbies" name="hobbies" type="text" autoComplete="hobbies" required className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={input.hobbies} onChange={handleChange} />
+                                </div>
+                            </div>
+
+                            <div className="w-1/3 px-3">
+                                <label htmlFor="address" className="block text-sm font-medium leading-6 text-gray-900">Address</label>
+                                <div className="mt-2">
+                                    <textarea id="address" name="address" rows="2" className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={input.address} onChange={handleChange}></textarea>
+                                </div>
                             </div>
                         </div>
+                        <div className='mt-6 px-3'>
+                            <button type="submit" className="rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-700">Submit</button>
 
-                        <div className='w-1/2 px-3'>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-                            <div className="mt-2">
-                                <input id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={input.email} onChange={handleChange} />
-                            </div>
+                            <button type='reset' className="ml-3 rounded-md bg-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">Reset</button>
                         </div>
+                    </form>
+                </div>
 
-                        <div className='w-1/2 px-3'>
-                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                            <div className="mt-2">
-                                <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 px-3 text px-3-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={input.password} onChange={handleChange} />
-                            </div>
-                        </div>
-
-                        <div className='w-1/2 px-3'>
-                            <label htmlFor="confirmPassword" className="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
-                            <div className="mt-2">
-                                <input id="confirm-password" name="confirmPassword" type="password" required className="block w-full rounded-md border-0 py-1.5 px-3 text-g px-3ray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={input.confirmPassword} onChange={handleChange} />
-                            </div>
-                        </div>
-
-                        <div className='w-1/3 px-3'>
-                            <label htmlFor="mobile" className="block text-sm font-medium leading-6 text-gray-900">Mobile no.</label>
-                            <div className="mt-2">
-                                <input id="mobile" name="mobile" type="text" required className="block w-full rounded-md border-0 py-1.5 px-3 text-g px-3ray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={input.mobile} onChange={handleChange} />
-                            </div>
-                        </div>
-
-                        <div className='w-1/3 px-3'>
-                            <label htmlFor="course" className="block text-sm font-medium leading-6 text-gray-900">Course</label>
-                            <div className="mt-2">
-                                <select name='course' className='block w-full rounded-md border-0 py-1.5 px-3 text-g px-3ray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' value={input.course} onChange={handleChange} >
-                                    <option value="">--select course--</option>
-                                    <option value="be">B.E</option>
-                                    <option value="bcom">B.COM</option>
-                                    <option value="bca">BCA</option>
-                                    <option value="ba">BA</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        <div className='w-1/3 px-3'>
-                            <label htmlFor="gender" className="block text-sm font-medium leading-6 text-gray-900">Gender</label>
-                            <div className="mt-2">
-                                Male
-                                <input id="male" name="gender" value='male' type="radio" required className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600 mx-3" onChange={handleChange} />
-                                Female
-                                <input id="female" name="gender" value='female' type="radio" required className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600 mx-3" onChange={handleChange} />
-                            </div>
-                        </div>
-
-                        <div className='w-1/3 px-3'>
-                            <label htmlFor="state" className="block text-sm font-medium leading-6 text-gray-900">State</label>
-                            <div className="mt-2">
-                                <DropDown input={input.state} handleOption={handleOption} states={states.states} name='state' />
-                            </div>
-                        </div>
-
-                        <div className='w-1/3 px-3'>
-                            <label htmlFor="state" className="block text-sm font-medium leading-6 text-gray-900">City</label>
-                            <div className="mt-2">
-                                <DropDown input={input.city} handleOption={handleOption} name='city' states={cities} />
-                            </div>
-                        </div>
-
-                        <div className='w-1/3 px-3'>
-                            <label htmlFor="hobbies" className="block text-sm font-medium leading-6 text-gray-900">hobbies</label>
-                            <div className="mt-2">
-                                <input id="hobbies" name="hobbies" type="text" autoComplete="hobbies" required className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={input.hobbies} onChange={handleChange} />
-                            </div>
-                        </div>
-
-                        <div className="w-1/3 px-3">
-                            <label htmlFor="address" className="block text-sm font-medium leading-6 text-gray-900">Address</label>
-                            <div className="mt-2">
-                                <textarea id="address" name="address" rows="2" className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={input.address} onChange={handleChange}></textarea>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='mt-6 px-3'>
-                        <button type="submit" className="rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-700">Submit</button>
-
-                        <button type='reset' className="ml-3 rounded-md bg-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">Reset</button>
-                    </div>
-                </form>
+                <Table data={data} handleEdit={handleEdit} handleDelete={handleDelete} isEdit={isEdit} />
             </div>
-
-            <Table data={data} handleEdit={handleEdit} handleDelete={handleDelete} isEdit={isEdit} />
         </>
     )
 }
--- src/PR7/components/Table.js
+++ src/PR7/components/Table.js
@@ -3,20 +3,36 @@ import React from 'react'
 const Table = ({ data, handleEdit, handleDelete, isEdit }) => {
     return (
 
-        <div className="relative overflow-x-auto">
-            <table className="w-full text-sm text-left ">
-                <thead className="text-xs  uppercase border-b dark:border-gray-700">
+        <div className="relative overflow-x-auto flex flex-wrap px-3">
+            <table className="table-fixed text-sm text-left  whitespace-nowrap flex-1">
+                <thead className="text-xs  uppercase border-b dark:border-gray-700 bg-indigo-500">
                     <tr>
-                        <th scope="col" className="px-6 py-3">Name</th>
-                        <th scope="col" className="px-6 py-3">Email</th>
-                        <th scope="col" className="px-6 py-3">Actions</th>
+                        <th scope="col" className="px-6 py-3 border-r border-gray-500">Name</th>
+                        <th scope="col" className="px-6 py-3 border-r border-gray-500">Email</th>
+                        <th scope="col" className="px-6 py-3 border-r border-gray-500">Password</th>
+                        <th scope="col" className="px-6 py-3 border-r border-gray-500">Gender</th>
+                        <th scope="col" className="px-6 py-3 border-r border-gray-500">Mobile</th>
+                        <th scope="col" className="px-6 py-3 border-r border-gray-500">Course</th>
+                        <th scope="col" className="px-6 py-3 border-r border-gray-500">State</th>
+                        <th scope="col" className="px-6 py-3 border-r border-gray-500">City</th>
+                        <th scope="col" className="px-6 py-3 border-r border-gray-500">Hobbies</th>
+                        <th scope="col" className="px-6 py-3 border-r border-gray-500">Address</th>
+                        <th colSpan={2} scope="col" className="px-6 py-3">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     {data.map((e, i) =>
                         <tr className="bg-white border-b" key={i}>
-                            <td className="px-6 py-4">{e.name}</td>
-                            <td className="px-6 py-4">{e.email}</td>
+                            <td className="px-6 py-4 border-r border-gray-500">{e.name}</td>
+                            <td className="px-6 py-4 border-r border-gray-500">{e.email}</td>
+                            <td className="px-6 py-4 border-r border-gray-500">{e.password}</td>
+                            <td className="px-6 py-4 border-r border-gray-500">{e.gender}</td>
+                            <td className="px-6 py-4 border-r border-gray-500">{e.mobile}</td>
+                            <td className="px-6 py-4 border-r border-gray-500">{e.course}</td>
+                            <td className="px-6 py-4 border-r border-gray-500">{e.state}</td>
+                            <td className="px-6 py-4 border-r border-gray-500">{e.city}</td>
+                            <td className="px-6 py-4 border-r border-gray-500">{e.hobbies}</td>
+                            <td className="px-6 py-4 border-r border-gray-500">{e.address}</td>
                             <td className="px-6 py-4">
                                 <button className='bg-blue-500 px-3 py-1 rounded' onClick={() => handleEdit(i)}>Edit</button>
                                 <button disabled={isEdit} className='bg-red-500 disabled:opacity-75 disabled:cursor-not-allowed px-3 py-1 rounded ml-2' onClick={() => handleDelete(i)}>Delete</button>
@@ -25,6 +41,7 @@ const Table = ({ data, handleEdit, handleDelete, isEdit }) => {
                     )}
                 </tbody>
             </table>
+            {data.length < 1 ? <h3 className='text-lg text-gray-600 text-center mt-3'>No data found</h3> : ''}
         </div>
 
     )
